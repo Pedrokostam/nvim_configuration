ca4558222d8817bd8a78983ad7d64d9dc5eddae2
Maciej Krosta <PedroKostam@outlook.com>
2026-04-24 16:36:41 +0200

    neovide background

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,14 +1,14 @@
 if vim.g.neovide then
    vim.o.guifont = "FantasqueSansM Nerd Font Mono:h14"
    vim.g.neovide_cursor_animation_length = 0.120
-vim.g.neovide_cursor_short_animation_length = 0.95
-vim.g.neovide_cursor_animate_command_line = false
-vim.g.neovide_padding_top = 0
-vim.g.neovide_padding_bottom = 0
-vim.g.neovide_padding_right = 0
-vim.g.neovide_padding_left = 0
-vim.g.neovide_scroll_animation_length = 0.1
-vim.g.neovide_position_animation_length = 0.1
-vim.g.neovide_scroll_animation_far_lines = 1
-
+   vim.opt.background='dark'
+   vim.g.neovide_cursor_short_animation_length = 0.95
+   vim.g.neovide_cursor_animate_command_line = false
+   vim.g.neovide_padding_top = 0
+   vim.g.neovide_padding_bottom = 0
+   vim.g.neovide_padding_right = 0
+   vim.g.neovide_padding_left = 0
+   vim.g.neovide_scroll_animation_length = 0.1
+   vim.g.neovide_position_animation_length = 0.1
+   vim.g.neovide_scroll_animation_far_lines = 1
 end
