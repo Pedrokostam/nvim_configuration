2d5db0d800fd6ee1476e03f05b8fc3e700f00441
Maciej Krosta <PedroKostam@outlook.com>
2026-07-14 18:46:58 +0200

    fixed which-key scroll

--- lua/plugins/which.lua
+++ lua/plugins/which.lua
@@ -7,6 +7,10 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
+    keys = {
+    scroll_down = "<C-PageDown>",
+    scroll_up = "<C-PageUp>",
+    },
   },
   keys = {
     -- {
