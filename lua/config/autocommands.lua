be01222956de464dee082140cd57c9eab480b6b8
Maciej Krosta <PedroKostam@outlook.com>
2026-08-17 21:51:39 +0200

    added surrounds

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -193,6 +193,63 @@ local function open_lsp_info()
    vim.cmd("stopinsert")
    vim.schedule(show_lsp_info)
 end
+
+vim.api.nvim_create_user_command("SurroundVisual", function(opts)
+	local args = opts.args
+	local open, close
+
+	if #args == 1 then
+		open = args
+		close = args
+	else
+		open = args:sub(1, 1)
+		close = args:sub(2, 2)
+	end
+
+	local buf = 0
+
+	local start = vim.api.nvim_buf_get_mark(buf, "<")
+	local finish = vim.api.nvim_buf_get_mark(buf, ">")
+
+	local start_row, start_col = start[1] - 1, start[2]
+	local end_row, end_col = finish[1] - 1, finish[2]
+
+	local text = vim.api.nvim_buf_get_text(
+		buf,
+		start_row,
+		start_col,
+		end_row,
+		end_col + 1,
+		{}
+	)
+
+	text[1] = open .. text[1]
+	text[#text] = text[#text] .. close
+
+	vim.api.nvim_buf_set_text(
+		buf,
+		start_row,
+		start_col,
+		end_row,
+		end_col + 1,
+		text
+	)
+end, {
+	nargs = 1,
+	range = true,
+})
+--
+-- Single 'character' input (uses the same character for both sides)
+vim.keymap.set("x", '\\"', ':SurroundVisual "<CR>', { silent = true, desc="Surround with single quote" })
+vim.keymap.set("x", "\\'", ":SurroundVisual '<CR>", { silent = true, desc="Surround with double quotes" })
+vim.keymap.set("x", "\\,", ":SurroundVisual ,<CR>", { silent = true, desc="Surround with commas" })
+
+-- Two character input (char 1 for left, char 2 for right)
+vim.keymap.set("x", "\\(", ":SurroundVisual ()<CR>", { silent = true, desc="Surround with paretheses" })
+vim.keymap.set("x", "\\[", ":SurroundVisual []<CR>", { silent = true, desc="Surround with brackets" })
+vim.keymap.set("x", "\\{", ":SurroundVisual {}<CR>", { silent = true, desc="Surround with curly braces" })
+vim.keymap.set("x", "\\<", ":SurroundVisual <><CR>", { silent = true, desc="Surround with angle brackets" })
+
 -- g? works everywhere; <C-?> only reaches Neovim in a GUI (terminals collapse it to <BS>)
 vim.keymap.set({ "n", "x" }, "g?", open_lsp_info, { desc = "Show LSP info & shortcuts", silent = true })
 vim.keymap.set({ "n", "i", "v" }, "<C-?>", open_lsp_info, { desc = "Show LSP info & shortcuts", silent = true })
